282695d548cbcfbf2989f870fad776572e0d370e
Mark Subit <marksubit@gmail.com>
2023-12-16 21:10:32 -0500

    angular crud tutorial

---
+++ BookStoreApi/Program.cs
@@ -0,0 +1,11 @@
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
+
+
